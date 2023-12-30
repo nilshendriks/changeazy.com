55a4e7a7dabc500873c588fc901d2dd2f95e8160
nilshendriks <info@nirusu.me>
2023-12-30 16:04:33 +0100

    initial setup

---
+++ src/data/navData.ts
@@ -0,0 +1,22 @@
+export const navData = [
+  {
+    name: "coaching",
+    href: "/#coaching"
+  },
+  {
+    name: "trainings",
+    href: "/#trainings"
+  },
+  {
+    name: "bodywork",
+    href: "/#bodywork"
+  },
+  {
+    name: "about",
+    href: "/#about"
+  },
+  {
+    name: "testimonials",
+    href: "/#testimonials"
+  }
+]
