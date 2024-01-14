43ae84c7f6b081e526e34a710ae73b73160886a2
nilshendriks <info@nirusu.me>
2024-01-14 19:50:04 +0100

    cleanup

---
+++ .netlify/functions-internal/ssr/chunks/generic_N5VMHSe7.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/generic_IYv5bEZQ.mjs');
+
+export { page };
